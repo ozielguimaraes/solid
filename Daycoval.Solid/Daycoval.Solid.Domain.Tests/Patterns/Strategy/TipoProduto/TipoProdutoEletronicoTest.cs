f8cdb6b26111d27c98759e06267f684338aaa146
Oziel Guimaraes de Paula Silva <so0005@daycoval.daycovalnet.com.br>
2019-03-18 16:55:17 -0300

    Adicionado teste para calculo de imposto para tipo de produtos IGUAL Superfulos

---
+++ Daycoval.Solid/Daycoval.Solid.Domain.Tests/Patterns/Strategy/TipoProduto/TipoProdutoEletronicoTest.cs
@@ -0,0 +1,26 @@
+﻿using Daycoval.Solid.Domain.Entidades;
+using Daycoval.Solid.Domain.Patterns.Strategy.TipoProduto;
+using Xunit;
+
+namespace Daycoval.Solid.Domain.Tests.Patterns.Strategy.TipoProduto
+{
+  public class TipoProdutoEletronicoTest
+    {
+        [Fact]
+        public void CalculandoValorImpostoEletronicoCorretamente()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Iphone X",
+                Quantidade = 1,
+                TipoProduto = Entidades.TipoProduto.Eletronico,
+                Valor = 3650
+            };
+
+            var tipoProdutoStrategy = new TipoProdutoEletronico();
+            produto.ValorImposto = tipoProdutoStrategy.CalcularValorImposto(produto.Valor);
+
+            Assert.True(produto.ValorImposto == 547.5m);
+        }
+    }
+}
---
+++ Daycoval.Solid/Daycoval.Solid.Domain.Tests/Patterns/Strategy/TipoProduto/TipoProdutoSuperfulosTest.cs
@@ -0,0 +1,26 @@
+﻿using Daycoval.Solid.Domain.Entidades;
+using Daycoval.Solid.Domain.Patterns.Strategy.TipoProduto;
+using Xunit;
+
+namespace Daycoval.Solid.Domain.Tests.Patterns.Strategy.TipoProduto
+{
+   public  class TipoProdutoSuperfulosTest
+    {
+        [Fact]
+        public void CalculandoValorImpostoSuperfulosCorretamente()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Headphone megapower",
+                Quantidade = 1,
+                TipoProduto = Entidades.TipoProduto.Superfulos,
+                Valor = 999
+            };
+
+            var tipoProdutoStrategy = new TipoProdutoSuperfulos();
+            produto.ValorImposto = tipoProdutoStrategy.CalcularValorImposto(produto.Valor);
+
+            Assert.True(produto.ValorImposto == 199.8m);
+        }
+    }
+}
