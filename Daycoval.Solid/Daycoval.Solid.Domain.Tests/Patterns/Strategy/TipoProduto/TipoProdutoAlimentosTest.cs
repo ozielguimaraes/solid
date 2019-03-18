62a619852c1efc692985e91f1a182200c24759a1
Oziel Guimaraes de Paula Silva <so0005@daycoval.daycovalnet.com.br>
2019-03-18 16:45:52 -0300

    Adicionado teste para calculo de imposto para tipo de produtos IGUAL alimentos

---
+++ Daycoval.Solid/Daycoval.Solid.Domain.Tests/Patterns/Strategy/TipoProduto/TipoProdutoAlimentosTest.cs
@@ -0,0 +1,26 @@
+﻿using Daycoval.Solid.Domain.Entidades;
+using Daycoval.Solid.Domain.Patterns.Strategy.TipoProduto;
+using Xunit;
+
+namespace Daycoval.Solid.Domain.Tests.Patterns.Strategy.TipoProduto
+{
+    public class TipoProdutoAlimentosTest
+    {
+        [Fact]
+        public void CalculandoValorImpostoAlimentosCorretamente()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Arroz tipo 1",
+                Quantidade = 1,
+                TipoProduto = Entidades.TipoProduto.Alimentos,
+                Valor = 15
+            };
+
+            var asds = new TipoProdutoAlimentos();
+            produto.ValorImposto = asds.CalcularValorImposto(produto.Valor);
+
+            Assert.True(produto.ValorImposto == 0.75m);
+        }
+    }
+}
